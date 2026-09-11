faf0d705a6394734aa0cd69e908b7b457fae356a
Felipe <felipeeh1524@gmail.com>
2026-09-11 17:08:54 -0300

    definir relacionamento entre models

--- catalog api/Models/Categoria.cs
+++ catalog api/Models/Categoria.cs
@@ -1,8 +1,16 @@
-﻿namespace catalog_api.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace catalog_api.Models;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
